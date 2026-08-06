2002fd216edee6e2a757c8b9f3ed323da5213249
Antony Delgado <delgadoantony95@gmail.com>
2026-08-05 20:46:09 -0400

    final hecho

--- modulos/generador_video.py
+++ modulos/generador_video.py
@@ -1,6 +1,6 @@
 import random
 import json
-from moviepy import VideoFileClip, AudioFileClip, CompositeVideoClip, ImageClip, ColorClip, AudioClip, VideoClip, vfx, afx, TextClip
+from moviepy import VideoFileClip, AudioFileClip, CompositeVideoClip, ImageClip, ColorClip, AudioClip, VideoClip, vfx, afx, TextClip, CompositeAudioClip
 
 #========================IMAGENES DE INTRO========================
 
@@ -79,7 +79,7 @@ def animacion(x_final, y_final, ancho, duracion_entrada=0.5):
 imagen_intro = ImageClip(obtener_imagen_intro_aleatoria())
 imagen_biblia = ImageClip(r"C:\Users\Antony\Desktop\Trivia\imagenes\intro\biblia.png")
 frase_audio_intro = AudioFileClip(obtener_ruta_audio_intro())
-duracion_total = frase_audio_intro.duration + 2  # Duración total del video de intro
+duracion_total_intro = frase_audio_intro.duration + 1  # Duración total del video de intro
 
 ANCHO, ALTO = 1080, 1920
 canva = ColorClip(size=(ANCHO, ALTO), color=(255, 255, 255))
@@ -90,12 +90,12 @@ canva = ColorClip(size=(ANCHO, ALTO), color=(255, 255, 255))
 # 
 #                                                    
 #--- canva fondo de intro negro
-canva = canva.with_duration(duracion_total)
+canva = canva.with_duration(duracion_total_intro)
 #
 
 #---------- imagen intro
 intro_w, intro_h = imagen_intro.size
-imagen_intro = imagen_intro.with_duration(duracion_total)
+imagen_intro = imagen_intro.with_duration(duracion_total_intro)
 imagen_intro = imagen_intro.with_fps(30).resized(height=ALTO, width=ANCHO).with_position(("center", "center"))
 
 #--------------- imagen de la biblia
@@ -111,7 +111,7 @@ animacion_biblia_intro = animacion(
 
 imagen_biblia = (
     imagen_biblia
-    .with_duration(duracion_total)
+    .with_duration(duracion_total_intro)
     .with_fps(30)
 )
 
@@ -130,7 +130,7 @@ texto_intro = TextClip(
     text_align="center",
 )
 
-texto_intro = texto_intro.with_duration(duracion_total).with_fps(30)
+texto_intro = texto_intro.with_duration(duracion_total_intro).with_fps(30)
 w_texto_intro, _ = texto_intro.size
 animacion_texto_intro = animacion(
     x_final = 34,
@@ -142,10 +142,19 @@ animacion_texto_intro = animacion(
 texto_intro = texto_intro.with_position(animacion_texto_intro)
 
 #---------------------audios de intro
+frase_audio_intro = frase_audio_intro
+wosh_intro_1 = AudioFileClip(obtener_audio_wosh_aleatorio())
+wosh_intro_1 = wosh_intro_1.with_start(0.10)
+
+audio_intro = CompositeAudioClip(
+    [
+    frase_audio_intro,
+    wosh_intro_1
+    ]
+)
 
 #----------------final de intro
-
-final = CompositeVideoClip(
+final_intro = CompositeVideoClip(
     [
     canva,
     imagen_intro,
@@ -153,6 +162,75 @@ final = CompositeVideoClip(
     texto_intro
     ]
     )
-final.save_frame("preview_intro.png", t=3)  # Guardar un fotograma de vista previa
+final_intro = final_intro.with_audio(audio_intro)
+#final_intro.save_frame("preview_intro.png", t=3)  # Guardar un fotograma de vista previa
 #final.preview(fps=15)
-final.write_videofile(r"C:\Users\Antony\Desktop\Trivia\marca\result.mp4")# Previsualizar la imagen de la biblia con efecto
+#final_intro.write_videofile(r"C:\Users\Antony\Desktop\Trivia\marca\result.mp4")# Previsualizar la imagen de la biblia con efecto
+
+
+# =========================================================================================================
+#                                         tercera parte 
+#                                            final
+#==========================================================================================================
+
+
+#-------------------importamos  clip de video y audios necesarios
+
+end_video = VideoFileClip(r"C:\Users\Antony\Desktop\Trivia\imagenes\end\fondo_final.gif")
+audio_voz_chao = AudioFileClip(r"C:\Users\Antony\Desktop\Trivia\audios\final\final_voz.mp3")
+
+duracion_end = audio_voz_chao.duration + 1  # Duración total del video 
+end_video = end_video.with_fps(30).resized(height=ALTO, width=ANCHO)
+end_video = end_video.with_effects([vfx.Loop(duration=duracion_end)])
+end_video = end_video.with_position(("center", "center"))
+
+#____________________________________________texto final
+texto_end = TextClip(
+    font=fuente,
+    text="''Comenta tu Puntuación''",
+    font_size=120,
+    color="white",
+    stroke_color="black",
+    stroke_width=5,
+    method="caption",
+    size=(1013, 332),
+    text_align="center",
+)
+
+texto_end = texto_end.with_duration(duracion_end).with_fps(30)
+w_texto_end, _ = texto_end.size
+
+animacion_texto_end = animacion(
+    x_final = 34,
+    y_final = 725,
+    ancho = w_texto_end,
+    duracion_entrada = 0.2,
+)
+
+texto_end = texto_end.with_position(animacion_texto_end)
+#_________________________________________________________________audio
+ 
+efecto_wosh_final = AudioFileClip(obtener_audio_wosh_aleatorio())
+efecto_wosh_final = efecto_wosh_final.with_start(0.10)
+
+audio_end = CompositeAudioClip([audio_voz_chao, efecto_wosh_final ])
+end_video = end_video.with_effects([vfx.Resize(new_size=(1080, 1920))])
+#_____________ ecportar
+end = CompositeVideoClip(
+    [
+    end_video,
+    texto_end
+    ])
+
+#end.preview(fps=15)
+end = end.with_audio(audio_end)
+end.write_videofile(
+    "resultado_final.mp4",
+    fps=24,
+    codec="libx264",                      # Códec compatible
+    preset="medium",                      # Asegura una codificación estándar
+    audio_codec="aac", 
+    ffmpeg_params=["-pix_fmt", "yuv420p"] # Formato de color compatible con Windows
+)
+#end.save_frame("preview_end.png", t=3)
+#mañaa arreglo directorios para funcionar en cualquier pc
